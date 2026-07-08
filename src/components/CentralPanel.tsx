177c271237f3ec7f6c69f7b539667364490a26da
Juan Carlos <juanrov_@hotmail.com>
2026-07-07 21:27:56 -0300

    fix: use react-resizable-panels v4 API (Group/Panel/Separator)

--- src/components/CentralPanel.tsx
+++ src/components/CentralPanel.tsx
@@ -1,22 +1,22 @@
-import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { Group, Panel, Separator } from "react-resizable-panels";
 import { Sidebar } from "./Sidebar";
 import { RequestPanel } from "./request/RequestPanel";
 import { ResponsePlaceholder } from "./response/ResponsePlaceholder";
 
 export function CentralPanel() {
   return (
-    <PanelGroup direction="horizontal" autoSaveId="hex-central">
+    <Group orientation="horizontal" id="hex-central" className="flex h-full w-full">
       <Panel defaultSize={20} minSize={12} maxSize={40}>
         <Sidebar />
       </Panel>
-      <PanelResizeHandle className="w-[1px] bg-border hover:bg-primary/40 transition-colors" />
+      <Separator className="w-[1px] bg-border hover:bg-primary/40 transition-colors" />
       <Panel defaultSize={48} minSize={30}>
         <RequestPanel />
       </Panel>
-      <PanelResizeHandle className="w-[1px] bg-border hover:bg-primary/40 transition-colors" />
+      <Separator className="w-[1px] bg-border hover:bg-primary/40 transition-colors" />
       <Panel defaultSize={32} minSize={20}>
         <ResponsePlaceholder />
       </Panel>
-    </PanelGroup>
+    </Group>
   );
 }

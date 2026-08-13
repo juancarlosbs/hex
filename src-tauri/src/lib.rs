a51946b1ddad1f5486c3d0527c3cc4cf04d73cd0
Juan Carlos <juanrov_@hotmail.com>
2026-08-13 08:22:16 -0300

    chore: add Tauri dialog and fs plugins for local file import

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -58,6 +58,8 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(builder.invoke_handler())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
